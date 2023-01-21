c239c20a7126832a61720ab5bdc8c63c5b041d9b
Evgenii Petrov <blinunleius@gmail.com>
2023-01-21 20:07:21 +0000

    Prove lemma_layoffunique

---
+++ lemma_supporting_lt.v
@@ -0,0 +1,25 @@
+Require Import ProofCheckingEuclid.euclidean_axioms.
+Require Import ProofCheckingEuclid.euclidean_defs.
+
+Section Euclid.
+
+Context `{Ax:euclidean_neutral_ruler_compass}.
+
+Lemma lemma_supporting_lt :
+	forall A B C D X,
+	BetS C X D ->
+	Cong C X A B ->
+	Lt A B C D.
+Proof.
+	intros A B C D X.
+	intros BetS_C_X_D.
+	intros Cong_CX_AB.
+
+	unfold Lt.
+	exists X.
+	split.
+	exact BetS_C_X_D.
+	exact Cong_CX_AB.
+Qed.
+
+End Euclid.
