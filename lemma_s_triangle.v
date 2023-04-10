41ad04c3d3ca367be076057261eafb99bf3c39ab
Evgenii Petrov <blinunleius@gmail.com>
2023-04-10 15:36:04 +0100

    Prove proposition 16

---
+++ lemma_s_triangle.v
@@ -0,0 +1,20 @@
+Require Import ProofCheckingEuclid.euclidean_axioms.
+Require Import ProofCheckingEuclid.euclidean_defs.
+
+Section Euclid.
+
+Context `{Ax:euclidean_neutral_ruler_compass}.
+
+Lemma lemma_s_triangle :
+	forall A B C,
+	nCol A B C ->
+	Triangle A B C.
+Proof.
+	intros A B C.
+	intros nCol_A_B_C.
+
+	unfold Triangle.
+	exact nCol_A_B_C.
+Qed.
+
+End Euclid.
