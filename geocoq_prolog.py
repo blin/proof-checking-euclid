1888396abd5e4e12ffe5008cc5e3118c6eedf48b
Evgenii Petrov <blinunleius@gmail.com>
2023-06-04 11:00:51 +0100

    Add scripts to import GeoCoq files

---
+++ geocoq_prolog.py
@@ -0,0 +1,334 @@
+import io
+import itertools
+from pprint import pprint
+from typing import Tuple
+
+from pyswip import Functor, Prolog
+from pyswip.easy import Functor
+
+from geocoq_rpl import (
+    Lemma,
+    Prop,
+    PropSimple,
+    PropExists,
+    PropInversion,
+    PropConjunction,
+    PropDisjunction,
+    collect_conjunction_nodes,
+    parse_all_lemmas,
+    process_parse_tree,
+)
+
+
+def prolog_prop(p: Prop) -> str:
+    match p:
+        case PropSimple():
+            ps = ",".join([f'"{pt}"' for pt in p.points])
+            return f'prop("{p.head}",[{ps}])'
+        case PropInversion():
+            ps = ",".join([f'"{pt}"' for pt in p.p.points])
+            return f'prop("n_{p.p.head}",[{ps}])'
+        case _:
+            raise ValueError(f"what: {p}")
+
+
+def prolog_lemma_rule(p: Prop, idp: str, p_to_i: dict[str, int]) -> str:
+    r = io.StringIO()
+    conclusion = idp[0] == "C"
+    r.write(f"{idp} = prop({idp}H,{idp}P),\n")
+    if not conclusion:
+        r.write(f"prop({idp}H,{idp}P),\n")
+
+    points = []
+    match p:
+        case PropSimple():
+            r.write(f'{idp}H = "{p.head}",\n')
+            points = p.points
+
+        case PropInversion():
+            if p.p.head == "Col":
+                r.write(f'{idp}H = "n{p.p.head}",\n')
+            else:
+                r.write(f'{idp}H = "n_{p.p.head}",\n')
+            points = p.p.points
+
+        case PropExists():
+            p = p.p
+            r.write(f'{idp}H = "{p.head}",\n')
+            points = p.points
+        case _:
+            raise ValueError(f"prolog_lemma_rule: {p}")
+    i = max(itertools.chain([0], p_to_i.values())) + 1
+    for p in points:
+        if p in p_to_i:
+            continue
+        p_to_i[p] = i
+        i += 1
+    ps = [f"P{p_to_i[p]}" for p in points]
+    pss = ",".join(ps)
+    r.write(f"{idp}P = [{pss}]")
+
+    return r.getvalue()
+
+
+def prolog_rule_for_lemma(req: Lemma) -> str:
+    r = io.StringIO()
+
+    req_name = req.name
+    match req.conclusion:
+        case PropSimple() | PropInversion():
+            conclusions = [req.conclusion]
+        case PropExists(points=_, p=PropSimple()):
+            conclusions = [req.conclusion.p]
+        case PropExists(points=_, p=PropConjunction()):
+            conclusions = collect_conjunction_nodes(req.conclusion.p)
+        case PropConjunction():
+            conclusions = collect_conjunction_nodes(req.conclusion)
+        case _:
+            raise ValueError(f"prolog_rule_for_lemma: {req}")
+    gids = [f"G{i+1}" for i in range(len(req.given))]
+    cids = [f"C{i+1}" for i in range(len(conclusions))]
+    q = ",".join(gids + cids)
+    r.write(f"{req_name}({q}) :-\n")
+
+    p_to_i = dict()
+    for i, p in enumerate(req.given):
+        r.write(prolog_lemma_rule(p, f"G{i+1}", p_to_i))
+        r.write(",\n")
+
+    match req.conclusion:
+        case PropSimple() | PropInversion():
+            r.write(prolog_lemma_rule(req.conclusion, f"C1", p_to_i))
+            r.write(".\n")
+        case PropExists(points=_, p=PropSimple()):
+            r.write(prolog_lemma_rule(req.conclusion.p, f"C1", p_to_i))
+            r.write(".\n")
+        case PropConjunction() | PropExists(points=_, p=PropConjunction()):
+            r.write(
+                ",\n".join(
+                    [prolog_lemma_rule(p, f"C{i+1}", p_to_i) for i, p in enumerate(conclusions)]
+                )
+            )
+            r.write(".\n")
+        case _:
+            raise ValueError(f"prolog_rule_for_lemma: {req}")
+
+    return r.getvalue()
+
+
+def prolog_lemmas(
+    req_names: list[str], lemmas_by_name: dict[str, Lemma], prolog_lemmas_path: str
+) -> None:
+    req_names = req_names - {
+        "Col",
+        "CongA",
+        "InCirc",
+        "Lt",
+        "LtA",
+        "OS",
+        "OnCirc",
+        "OnRay",
+        "OutCirc",
+        "SS",
+        "TT",
+        "Perp_at",
+        "Supp",
+        "Midpoint",
+        "RightTriangle",
+        "TogetherGreater",
+        "Triangle",
+        "axiom_betweennessidentity",
+        "axiom_betweennesssymmetry",
+        "cn_congruencereflexive",
+        "cn_congruencereverse",
+        "cn_equalityreflexive",
+        "cn_equalitysub",
+        "isosceles",
+        "lemma_crossbar",
+        "lemma_extension",
+        "lemma_onray_assert",
+        "lemma_onray_orderofpoints_any",
+        "postulate_circle_circle",
+        "proposition_22",
+    }
+
+    rr = open(prolog_lemmas_path, "w")
+    for req_name in sorted(req_names):
+        req = lemmas_by_name[req_name]
+        rr.write(prolog_rule_for_lemma(req))
+        rr.write("\n")
+    rr.close()
+
+
+def prolog_context(context: list[Prop], prolog_context_path: str) -> None:
+    with open(prolog_context_path, "w") as cf:
+        for cp in context:
+            if isinstance(cp, PropInversion) and isinstance(cp.p, PropDisjunction):
+                cp_d = cp.p
+                cf.write(prolog_prop(PropInversion(p=cp_d.left)))
+                cf.write(".\n")
+                if isinstance(cp_d.right, PropDisjunction):
+                    cf.write(prolog_prop(PropInversion(p=cp_d.right.left)))
+                    cf.write(".\n")
+                    cf.write(prolog_prop(PropInversion(p=cp_d.right.right)))
+                    cf.write(".\n")
+                else:
+                    cf.write(prolog_prop(PropInversion(p=cp_d.right)))
+                    cf.write(".\n")
+            else:
+                cf.write(prolog_prop(cp))
+                cf.write(".\n")
+
+
+def functor_to_var_prop(f: Functor) -> Tuple[str, PropSimple]:
+    assert isinstance(f, Functor)
+    assert f.name.get_value() == "="
+    var_name = f.args[0].get_value()
+    prop = f.args[1]
+
+    assert isinstance(prop, Functor)
+    assert prop.name.get_value() == "prop"
+
+    prop_name = prop.args[0].decode("utf-8")
+    points_raw = prop.args[1]
+    assert isinstance(points_raw, list)
+    points = [p.decode("utf-8") for p in points_raw]
+    return (var_name, PropSimple(head=prop_name, points=points))
+
+
+def prove_simple(
+    p: PropSimple, by: Lemma, prolog_lemmas_path: str, prolog_context_path: str
+) -> str:
+    prolog = Prolog()
+    prolog.consult(prolog_lemmas_path)
+    prolog.consult(prolog_context_path)
+
+    pq_given = ",".join([f"G{i+1}" for i in range(len(by.given))])
+    pq_args = f"{pq_given},{prolog_prop(p)}"
+    q = f"{by.name}({pq_args})"
+
+    answers = list(prolog.query(q, normalize=False))
+    if len(answers) < 1:
+        pprint(q)
+        pprint(answers)
+        pprint(open(prolog_context_path).read())
+        raise ValueError("no answers!")
+    answer = answers[0]
+    answer_vars: list[Tuple[str, PropSimple]] = list()
+    for f in answer:
+        var_name, prop = functor_to_var_prop(f)
+        answer_vars.append((var_name, prop))
+    pp = " ".join(["_" for i in range(len(by.points))])
+    gg = " ".join([e[1].to_var() for e in answer_vars])
+    return f"{by.name} {pp} {gg}"
+
+
+def prove_forward_using(
+    p: PropSimple, by: Lemma, prolog_lemmas_path: str, prolog_context_path: str
+) -> Tuple[str, int]:
+    prolog = Prolog()
+    prolog.consult(prolog_lemmas_path)
+    prolog.consult(prolog_context_path)
+
+    given = by.given
+    assert isinstance(by.conclusion, PropConjunction)
+    conclusions = collect_conjunction_nodes(by.conclusion)
+    gids = [f"G{i+1}" for i in range(len(given))]
+
+    for ci in range(len(conclusions)):
+        cids = [f"C{i+1}" for i in range(len(conclusions))]
+        cids[ci] = prolog_prop(p)
+        pq_given = ",".join(gids)
+        pg_conclusion = ",".join(cids)
+        pq_args = f"{pq_given},{pg_conclusion}"
+        q = f"{by.name}({pq_args})"
+
+        answers = list(prolog.query(q, normalize=False))
+        if len(answers) < 1:
+            continue
+        answer = answers[0]
+        answer_vars: list[Tuple[str, PropSimple]] = list()
+        for f in answer:
+            var_name, prop = functor_to_var_prop(f)
+            answer_vars.append((var_name, prop))
+        pp = " ".join(["_" for i in range(len(by.points))])
+        gg = " ".join([e[1].to_var() for e in answer_vars if e[0].startswith("G")])
+        return (f"{by.name} {pp} {gg}", ci)
+    pprint(q)
+    pprint(open(prolog_context_path).read())
+    raise ValueError("no answers in forward_using!")
+
+
+def prove_conclude_exists(
+    p: PropExists, by: Lemma, prolog_lemmas_path: str, prolog_context_path: str
+) -> Tuple[str, int]:
+    prolog = Prolog()
+    prolog.consult(prolog_lemmas_path)
+    prolog.consult(prolog_context_path)
+
+    given = by.given
+    assert isinstance(by.conclusion.p, PropConjunction)
+    conclusions = collect_conjunction_nodes(p.p)
+    gids = [f"G{i+1}" for i in range(len(given))]
+
+    cids = [prolog_prop(c) for c in conclusions]
+    pq_given = ",".join(gids)
+    pg_conclusion = ",".join(cids)
+    pq_args = f"{pq_given},{pg_conclusion}"
+    q = f"{by.name}({pq_args})"
+
+    answers = list(prolog.query(q, normalize=False))
+    if len(answers) < 1:
+        pprint(q)
+        pprint(answers)
+        pprint(open(prolog_context_path).read())
+        raise ValueError("no answers!")
+    answer = answers[0]
+    answer_vars: list[Tuple[str, PropSimple]] = list()
+    for f in answer:
+        var_name, prop = functor_to_var_prop(f)
+        answer_vars.append((var_name, prop))
+    pp = " ".join(["_" for i in range(len(by.points))])
+    gg = " ".join([e[1].to_var() for e in answer_vars if e[0].startswith("G")])
+    return f"{by.name} {pp} {gg}"
+
+
+def prove_conclude_conjunction(
+    p: PropConjunction, by: Lemma, prolog_lemmas_path: str, prolog_context_path: str
+) -> Tuple[str, int]:
+    prolog = Prolog()
+    prolog.consult(prolog_lemmas_path)
+    prolog.consult(prolog_context_path)
+
+    given = by.given
+    assert isinstance(by.conclusion, PropConjunction)
+    conclusions = collect_conjunction_nodes(p)
+    gids = [f"G{i+1}" for i in range(len(given))]
+
+    cids = [prolog_prop(c) for c in conclusions]
+    pq_given = ",".join(gids)
+    pg_conclusion = ",".join(cids)
+    pq_args = f"{pq_given},{pg_conclusion}"
+    q = f"{by.name}({pq_args})"
+
+    answers = list(prolog.query(q, normalize=False))
+    if len(answers) < 1:
+        pprint(q)
+        pprint(answers)
+        pprint(open(prolog_context_path).read())
+        raise ValueError("no answers!")
+    answer = answers[0]
+    answer_vars: list[Tuple[str, PropSimple]] = list()
+    for f in answer:
+        var_name, prop = functor_to_var_prop(f)
+        answer_vars.append((var_name, prop))
+    pp = " ".join(["_" for i in range(len(by.points))])
+    gg = " ".join([e[1].to_var() for e in answer_vars if e[0].startswith("G")])
+    return f"{by.name} {pp} {gg}"
+
+
+lemma_name = "lemma_TogetherGreater_flip"
+lemmas_by_name = parse_all_lemmas()
+
+# pprint(lemmas_by_name[lemma_name])
+# print(prolog_rule_for_lemma(lemmas_by_name[lemma_name]))
---
+++ lemma_pointreflectionisometry.v
@@ -0,0 +1,244 @@
+Require Import ProofCheckingEuclid.euclidean_axioms.
+Require Import ProofCheckingEuclid.euclidean_defs.
+Require Import ProofCheckingEuclid.lemma_ABCequalsCBA.
+Require Import ProofCheckingEuclid.lemma_NCdistinct.
+Require Import ProofCheckingEuclid.lemma_NCorder.
+Require Import ProofCheckingEuclid.lemma_betweennotequal.
+Require Import ProofCheckingEuclid.lemma_congruenceflip.
+Require Import ProofCheckingEuclid.lemma_congruencesymmetric.
+Require Import ProofCheckingEuclid.lemma_congruencetransitive.
+Require Import ProofCheckingEuclid.lemma_differenceofparts.
+Require Import ProofCheckingEuclid.lemma_doublereverse.
+Require Import ProofCheckingEuclid.lemma_equalanglesNC.
+Require Import ProofCheckingEuclid.lemma_equalanglestransitive.
+Require Import ProofCheckingEuclid.lemma_extensionunique.
+Require Import ProofCheckingEuclid.lemma_inequalitysymmetric.
+Require Import ProofCheckingEuclid.lemma_layoffunique.
+Require Import ProofCheckingEuclid.lemma_lessthancongruence.
+Require Import ProofCheckingEuclid.lemma_lessthancongruence_smaller.
+Require Import ProofCheckingEuclid.lemma_onray_ABC_ACB.
+Require Import ProofCheckingEuclid.lemma_onray_assert.
+Require Import ProofCheckingEuclid.lemma_orderofpoints_ABC_ACD_BCD.
+Require Import ProofCheckingEuclid.lemma_orderofpoints_ABC_BCD_ABD.
+Require Import ProofCheckingEuclid.lemma_orderofpoints_ABC_BCD_ACD.
+Require Import ProofCheckingEuclid.lemma_outerconnectivity.
+Require Import ProofCheckingEuclid.lemma_s_col_BetS_A_B_C.
+Require Import ProofCheckingEuclid.lemma_s_col_BetS_A_C_B.
+Require Import ProofCheckingEuclid.lemma_s_col_BetS_B_A_C.
+Require Import ProofCheckingEuclid.lemma_s_col_eq_A_B.
+Require Import ProofCheckingEuclid.lemma_s_col_eq_A_C.
+Require Import ProofCheckingEuclid.lemma_s_col_eq_B_C.
+Require Import ProofCheckingEuclid.lemma_s_isosceles.
+Require Import ProofCheckingEuclid.lemma_s_lt.
+Require Import ProofCheckingEuclid.lemma_s_n_col_ncol.
+Require Import ProofCheckingEuclid.lemma_s_ncol_n_col.
+Require Import ProofCheckingEuclid.lemma_s_ncol_triangle.
+Require Import ProofCheckingEuclid.lemma_s_oncirc_radius.
+Require Import ProofCheckingEuclid.lemma_s_onray.
+Require Import ProofCheckingEuclid.lemma_s_onray_assert_ABB.
+Require Import ProofCheckingEuclid.lemma_s_onray_assert_bets_ABE.
+Require Import ProofCheckingEuclid.lemma_s_onray_assert_bets_AEB.
+Require Import ProofCheckingEuclid.lemma_s_triangle.
+Require Import ProofCheckingEuclid.proposition_03.
+Require Import ProofCheckingEuclid.proposition_04.
+Require Import ProofCheckingEuclid.proposition_15a.
+
+Section Euclid.
+
+Context `{Ax:euclidean_neutral_ruler_compass}.
+
+Lemma lemma_pointreflectionisometry :
+	forall A B C P Q,
+	Midpoint A B C ->
+	Midpoint P B Q ->
+	neq A P ->
+	Cong A P C Q.
+Proof.
+	intros A B C P Q.
+	intros Midpoint_A_B_C.
+	intros Midpoint_P_B_Q.
+	intros neq_A_P.
+
+
+	destruct Midpoint_A_B_C as (BetS_A_B_C & Cong_A_B_B_C).
+	destruct Midpoint_P_B_Q as (BetS_P_B_Q & Cong_P_B_B_Q).
+
+	(* assert by cases *)
+	assert (Cong A P C Q) as Cong_A_P_C_Q.
+	assert (Col A B P \/ ~ Col A B P) as [Col_A_B_P|n_Col_A_B_P] by (apply Classical_Prop.classic).
+	{
+		(* case Col_A_B_P *)
+
+		(* assert by cases *)
+		assert (Cong A P C Q) as Cong_A_P_C_Q.
+		destruct Col_A_B_P as [eq_A_B | [eq_A_P | [eq_B_P | [BetS_B_A_P | [BetS_A_B_P | BetS_A_P_B]]]]].
+		{
+			(* case eq_A_B *)
+
+			assert (~ ~ Cong A P C Q) as n_n_Cong_A_P_C_Q.
+			{
+				intro n_Cong_A_P_C_Q.
+				pose proof (lemma_betweennotequal _ _ _ BetS_A_B_C) as (_ & neq_A_B & _).
+				contradict eq_A_B.
+				exact neq_A_B.
+			}
+			assert (Cong_A_P_C_Q := n_n_Cong_A_P_C_Q).
+			apply Classical_Prop.NNPP in Cong_A_P_C_Q.
+
+			exact Cong_A_P_C_Q.
+		}
+		{
+			(* case eq_A_P *)
+			contradict eq_A_P.
+			exact neq_A_P.
+		}
+		{
+			(* case eq_B_P *)
+
+			assert (~ ~ Cong A P C Q) as n_n_Cong_A_P_C_Q.
+			{
+				intro n_Cong_A_P_C_Q.
+				pose proof (lemma_betweennotequal _ _ _ BetS_P_B_Q) as (_ & neq_P_B & _).
+				pose proof (lemma_inequalitysymmetric _ _ neq_P_B) as neq_B_P.
+				contradict eq_B_P.
+				exact neq_B_P.
+			}
+			assert (Cong_A_P_C_Q := n_n_Cong_A_P_C_Q).
+			apply Classical_Prop.NNPP in Cong_A_P_C_Q.
+
+			exact Cong_A_P_C_Q.
+		}
+		{
+			(* case BetS_B_A_P *)
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_A_B_B_C) as Cong_B_C_A_B.
+			pose proof (lemma_congruenceflip _ _ _ _ Cong_B_C_A_B) as (_ & _ & Cong_B_C_B_A).
+			pose proof (lemma_doublereverse _ _ _ _ Cong_P_B_B_Q) as (Cong_Q_B_B_P & _).
+			pose proof (lemma_congruenceflip _ _ _ _ Cong_Q_B_B_P) as (_ & Cong_B_Q_B_P & _).
+			pose proof (lemma_doublereverse _ _ _ _ Cong_B_C_A_B) as (Cong_B_A_C_B & _).
+			pose proof (lemma_s_lt _ _ _ _ _ BetS_B_A_P Cong_B_A_C_B) as Lt_C_B_B_P.
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_B_Q_B_P) as Cong_B_P_B_Q.
+			pose proof (lemma_lessthancongruence _ _ _ _ _ _ Lt_C_B_B_P Cong_B_P_B_Q) as Lt_C_B_B_Q.
+			pose proof (cn_congruencereverse C B) as Cong_C_B_B_C.
+			pose proof (lemma_lessthancongruence_smaller _ _ _ _ _ _ Lt_C_B_B_Q Cong_C_B_B_C) as Lt_B_C_B_Q.
+			pose proof (cn_congruencereflexive B Q) as Cong_B_Q_B_Q.
+			pose proof (lemma_betweennotequal _ _ _ BetS_P_B_Q) as (neq_B_Q & _ & _).
+			pose proof (proposition_03 _ _ _ _ _ _ Lt_B_C_B_Q Cong_B_Q_B_Q) as (H & BetS_B_H_Q & Cong_B_H_B_C).
+			
+			pose proof (lemma_s_onray_assert_bets_AEB B Q H BetS_B_H_Q neq_B_Q) as OnRay_B_Q_H.
+
+			pose proof (axiom_betweennesssymmetry _ _ _ BetS_B_A_P) as BetS_P_A_B.
+			pose proof (lemma_orderofpoints_ABC_BCD_ACD _ _ _ _ BetS_P_A_B BetS_A_B_C) as BetS_P_B_C.
+			pose proof (lemma_s_onray _ _ _ _ BetS_P_B_C BetS_P_B_Q) as OnRay_B_C_Q.
+			pose proof (lemma_onray_ABC_ACB _ _ _ OnRay_B_C_Q) as OnRay_B_Q_C.
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_B_H_B_C) as Cong_B_C_B_H.
+			pose proof (lemma_layoffunique _ _ _ _ OnRay_B_Q_C OnRay_B_Q_H Cong_B_C_B_H) as eq_C_H.
+			assert (BetS B C Q) as BetS_B_C_Q by (rewrite eq_C_H; exact BetS_B_H_Q).
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_B_C_B_A) as Cong_B_A_B_C.
+			pose proof (lemma_differenceofparts _ _ _ _ _ _ Cong_B_A_B_C Cong_B_P_B_Q BetS_B_A_P BetS_B_C_Q) as Cong_A_P_C_Q.
+
+			exact Cong_A_P_C_Q.
+		}
+		{
+			(* case BetS_A_B_P *)
+
+			assert (~ ~ Cong A P C Q) as n_n_Cong_A_P_C_Q.
+			{
+				intro n_Cong_A_P_C_Q.
+
+				assert (~ BetS B P C) as n_BetS_B_P_C.
+				{
+					intro BetS_B_P_C.
+					pose proof (axiom_betweennesssymmetry _ _ _ BetS_B_P_C) as BetS_C_P_B.
+					pose proof (lemma_orderofpoints_ABC_BCD_ACD _ _ _ _ BetS_C_P_B BetS_P_B_Q) as BetS_C_B_Q.
+					pose proof (lemma_congruenceflip _ _ _ _ Cong_A_B_B_C) as (_ & _ & Cong_A_B_C_B).
+					pose proof (lemma_congruenceflip _ _ _ _ Cong_P_B_B_Q) as (_ & Cong_B_P_B_Q & _).
+					pose proof (cn_sumofparts _ _ _ _ _ _ Cong_A_B_C_B Cong_B_P_B_Q BetS_A_B_P BetS_C_B_Q) as Cong_A_P_C_Q.
+					contradict Cong_A_P_C_Q.
+					exact n_Cong_A_P_C_Q.
+				}
+
+
+				assert (~ BetS B C P) as n_BetS_B_C_P.
+				{
+					intro BetS_B_C_P.
+					pose proof (axiom_betweennesssymmetry _ _ _ BetS_P_B_Q) as BetS_Q_B_P.
+					pose proof (axiom_orderofpoints_ABD_BCD_ABC _ _ _ _ BetS_Q_B_P BetS_B_C_P) as BetS_Q_B_C.
+					pose proof (axiom_betweennesssymmetry _ _ _ BetS_Q_B_C) as BetS_C_B_Q.
+					pose proof (lemma_congruenceflip _ _ _ _ Cong_A_B_B_C) as (_ & _ & Cong_A_B_C_B).
+					pose proof (lemma_congruenceflip _ _ _ _ Cong_P_B_B_Q) as (_ & Cong_B_P_B_Q & _).
+					pose proof (cn_sumofparts _ _ _ _ _ _ Cong_A_B_C_B Cong_B_P_B_Q BetS_A_B_P BetS_C_B_Q) as Cong_A_P_C_Q.
+					contradict Cong_A_P_C_Q.
+					exact n_Cong_A_P_C_Q.
+				}
+
+				pose proof (lemma_outerconnectivity _ _ _ _ BetS_A_B_P BetS_A_B_C n_BetS_B_P_C n_BetS_B_C_P) as eq_P_C.
+				assert (Cong A B B P) as Cong_A_B_B_P by (rewrite eq_P_C; exact Cong_A_B_B_C).
+				pose proof (lemma_congruenceflip _ _ _ _ Cong_P_B_B_Q) as (_ & Cong_B_P_B_Q & _).
+				pose proof (lemma_congruencetransitive _ _ _ _ _ _ Cong_A_B_B_P Cong_B_P_B_Q) as Cong_A_B_B_Q.
+				pose proof (axiom_betweennesssymmetry _ _ _ BetS_A_B_C) as BetS_C_B_A.
+				assert (BetS P B A) as BetS_P_B_A by (rewrite eq_P_C; exact BetS_C_B_A).
+				pose proof (lemma_congruencesymmetric _ _ _ _ Cong_A_B_B_Q) as Cong_B_Q_A_B.
+				pose proof (lemma_congruenceflip _ _ _ _ Cong_B_Q_A_B) as (_ & _ & Cong_B_Q_B_A).
+				pose proof (lemma_extensionunique _ _ _ _ BetS_P_B_Q BetS_P_B_A Cong_B_Q_B_A) as eq_Q_A.
+				pose proof (cn_congruencereverse A C) as Cong_A_C_C_A.
+				assert (Cong A P C A) as Cong_A_P_C_A by (rewrite eq_P_C; exact Cong_A_C_C_A).
+				assert (Cong A P C Q) as Cong_A_P_C_Q by (rewrite eq_Q_A; exact Cong_A_P_C_A).
+				contradict Cong_A_P_C_Q.
+				exact n_Cong_A_P_C_Q.
+			}
+			assert (Cong_A_P_C_Q := n_n_Cong_A_P_C_Q).
+			apply Classical_Prop.NNPP in Cong_A_P_C_Q.
+
+
+			exact Cong_A_P_C_Q.
+		}
+		{
+			(* case BetS_A_P_B *)
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_P_B_B_Q) as Cong_B_Q_P_B.
+			pose proof (lemma_congruenceflip _ _ _ _ Cong_B_Q_P_B) as (_ & _ & Cong_B_Q_B_P).
+			pose proof (lemma_doublereverse _ _ _ _ Cong_A_B_B_C) as (Cong_C_B_B_A & _).
+			pose proof (lemma_congruenceflip _ _ _ _ Cong_C_B_B_A) as (_ & Cong_B_C_B_A & _).
+			pose proof (axiom_betweennesssymmetry _ _ _ BetS_A_P_B) as BetS_B_P_A.
+			pose proof (lemma_doublereverse _ _ _ _ Cong_B_Q_P_B) as (Cong_B_P_Q_B & _).
+			pose proof (lemma_s_lt _ _ _ _ _ BetS_B_P_A Cong_B_P_Q_B) as Lt_Q_B_B_A.
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_B_C_B_A) as Cong_B_A_B_C.
+			pose proof (lemma_lessthancongruence _ _ _ _ _ _ Lt_Q_B_B_A Cong_B_A_B_C) as Lt_Q_B_B_C.
+			pose proof (cn_congruencereverse Q B) as Cong_Q_B_B_Q.
+			pose proof (lemma_lessthancongruence_smaller _ _ _ _ _ _ Lt_Q_B_B_C Cong_Q_B_B_Q) as Lt_B_Q_B_C.
+			pose proof (cn_congruencereflexive B C) as Cong_B_C_B_C.
+			pose proof (lemma_betweennotequal _ _ _ BetS_A_B_C) as (neq_B_C & _ & _).
+			pose proof (proposition_03 _ _ _ _ _ _ Lt_B_Q_B_C Cong_B_C_B_C) as (H & BetS_B_H_C & Cong_B_H_B_Q).
+			pose proof (lemma_orderofpoints_ABC_ACD_BCD _ _ _ _ BetS_A_P_B BetS_A_B_C) as BetS_P_B_C.
+			pose proof (lemma_s_onray _ _ _ _ BetS_P_B_C BetS_P_B_Q) as OnRay_B_C_Q.
+			
+			pose proof (lemma_s_onray_assert_bets_AEB B C H BetS_B_H_C neq_B_C) as OnRay_B_C_H.
+
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_B_H_B_Q) as Cong_B_Q_B_H.
+			pose proof (lemma_layoffunique _ _ _ _ OnRay_B_C_Q OnRay_B_C_H Cong_B_Q_B_H) as eq_Q_H.
+			assert (BetS B Q C) as BetS_B_Q_C by (rewrite eq_Q_H; exact BetS_B_H_C).
+			pose proof (lemma_congruencesymmetric _ _ _ _ Cong_B_Q_B_P) as Cong_B_P_B_Q.
+			pose proof (lemma_differenceofparts _ _ _ _ _ _ Cong_B_P_B_Q Cong_B_A_B_C BetS_B_P_A BetS_B_Q_C) as Cong_P_A_Q_C.
+			pose proof (lemma_congruenceflip _ _ _ _ Cong_P_A_Q_C) as (Cong_A_P_C_Q & _ & _).
+
+			exact Cong_A_P_C_Q.
+		}
+
+		exact Cong_A_P_C_Q.
+	}
+	{
+		(* case n_Col_A_B_P *)
+		pose proof (lemma_s_n_col_ncol _ _ _ n_Col_A_B_P) as nCol_A_B_P.
+		pose proof (proposition_15a _ _ _ _ _ BetS_A_B_C BetS_P_B_Q nCol_A_B_P) as CongA_A_B_P_Q_B_C.
+		pose proof (lemma_equalanglesNC _ _ _ _ _ _ CongA_A_B_P_Q_B_C) as nCol_Q_B_C.
+		pose proof (lemma_ABCequalsCBA _ _ _ nCol_Q_B_C) as CongA_Q_B_C_C_B_Q.
+		pose proof (lemma_equalanglestransitive _ _ _ _ _ _ _ _ _ CongA_A_B_P_Q_B_C CongA_Q_B_C_C_B_Q) as CongA_A_B_P_C_B_Q.
+		pose proof (lemma_congruenceflip _ _ _ _ Cong_A_B_B_C) as (_ & Cong_B_A_B_C & _).
+		pose proof (lemma_congruenceflip _ _ _ _ Cong_P_B_B_Q) as (_ & Cong_B_P_B_Q & _).
+		pose proof (proposition_04 _ _ _ _ _ _ Cong_B_A_B_C Cong_B_P_B_Q CongA_A_B_P_C_B_Q) as (Cong_A_P_C_Q & _ & _).
+
+		exact Cong_A_P_C_Q.
+	}
+	exact Cong_A_P_C_Q.
+Qed.
+
+End Euclid.
