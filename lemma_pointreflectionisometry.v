ae438f390104b87a71282ae8db42275843035fb3
Evgenii Petrov <blinunleius@gmail.com>
2023-06-17 20:43:52 +0100

    Prove lemma_Euclid4

---
+++ lemma_pointreflectionisometry.v
@@ -0,0 +1,200 @@
+Require Import ProofCheckingEuclid.euclidean_axioms.
+Require Import ProofCheckingEuclid.euclidean_defs.
+Require Import ProofCheckingEuclid.lemma_ABCequalsCBA.
+Require Import ProofCheckingEuclid.lemma_betweennotequal.
+Require Import ProofCheckingEuclid.lemma_congruenceflip.
+Require Import ProofCheckingEuclid.lemma_congruencesymmetric.
+Require Import ProofCheckingEuclid.lemma_congruencetransitive.
+Require Import ProofCheckingEuclid.lemma_differenceofparts.
+Require Import ProofCheckingEuclid.lemma_doublereverse.
+Require Import ProofCheckingEuclid.lemma_equalanglesNC.
+Require Import ProofCheckingEuclid.lemma_equalanglestransitive.
+Require Import ProofCheckingEuclid.lemma_extensionunique.
+Require Import ProofCheckingEuclid.lemma_layoffunique.
+Require Import ProofCheckingEuclid.lemma_lessthancongruence.
+Require Import ProofCheckingEuclid.lemma_lessthancongruence_smaller.
+Require Import ProofCheckingEuclid.lemma_onray_ABC_ACB.
+Require Import ProofCheckingEuclid.lemma_orderofpoints_ABC_ACD_BCD.
+Require Import ProofCheckingEuclid.lemma_orderofpoints_ABC_BCD_ACD.
+Require Import ProofCheckingEuclid.lemma_outerconnectivity.
+Require Import ProofCheckingEuclid.lemma_s_lt.
+Require Import ProofCheckingEuclid.lemma_s_n_col_ncol.
+Require Import ProofCheckingEuclid.lemma_s_onray.
+Require Import ProofCheckingEuclid.lemma_s_onray_assert_bets_AEB.
+Require Import ProofCheckingEuclid.proposition_03.
+Require Import ProofCheckingEuclid.proposition_04.
+Require Import ProofCheckingEuclid.proposition_15a.
+
+Section Euclid.
+
+Context `{Ax:euclidean_neutral_ruler_compass}.
+
+(* Points A and P are reflected through point B. *)
+Lemma lemma_pointreflectionisometry :
+	forall A B C P Q,
+	Midpoint A B C ->
+	Midpoint P B Q ->
+	neq A P ->
+	Cong A P C Q.
+Proof.
+	intros A B C P Q.
+	intros Midpoint_A_B_C.
+	intros Midpoint_P_B_Q.
+	intros neq_A_P.
+
+	pose proof (cn_congruencereflexive B C) as Cong_BC_BC.
+	pose proof (cn_congruencereflexive B Q) as Cong_BQ_BQ.
+	pose proof (cn_congruencereverse A C) as Cong_AC_CA.
+	pose proof (cn_congruencereverse C B) as Cong_CB_BC.
+	pose proof (cn_congruencereverse Q B) as Cong_QB_BQ.
+
+	destruct Midpoint_A_B_C as (BetS_A_B_C & Cong_AB_BC).
+	destruct Midpoint_P_B_Q as (BetS_P_B_Q & Cong_PB_BQ).
+
+	pose proof (axiom_betweennesssymmetry _ _ _ BetS_A_B_C) as BetS_C_B_A.
+	pose proof (lemma_betweennotequal _ _ _ BetS_A_B_C) as (neq_B_C & neq_A_B & neq_A_C).
+
+	pose proof (axiom_betweennesssymmetry _ _ _ BetS_P_B_Q) as BetS_Q_B_P.
+	pose proof (lemma_betweennotequal _ _ _ BetS_P_B_Q) as (neq_B_Q & neq_P_B & neq_P_Q).
+	pose proof (lemma_betweennotequal _ _ _ BetS_Q_B_P) as (neq_B_P & neq_Q_B & neq_Q_P).
+
+	pose proof (lemma_congruencesymmetric _ _ _ _ Cong_AB_BC) as Cong_BC_AB.
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_AB_BC) as (_ & _ & Cong_AB_CB).
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_BC_AB) as (_ & _ & Cong_BC_BA).
+	pose proof (lemma_doublereverse _ _ _ _ Cong_BC_AB) as (Cong_BA_CB & _).
+	pose proof (lemma_doublereverse _ _ _ _ Cong_PB_BQ) as (Cong_QB_BP & _).
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_QB_BP) as (_ & Cong_BQ_BP & _).
+	pose proof (lemma_congruencesymmetric _ _ _ _ Cong_PB_BQ) as Cong_BQ_PB.
+	pose proof (lemma_congruencesymmetric _ _ _ _ Cong_BQ_BP) as Cong_BP_BQ.
+	pose proof (lemma_congruencesymmetric _ _ _ _ Cong_BC_BA) as Cong_BA_BC.
+	pose proof (lemma_doublereverse _ _ _ _ Cong_BQ_PB) as (Cong_BP_QB & _).
+
+	(* assert by cases *)
+	assert (~ Col A B P \/ ~ ~ Col A B P) as [n_Col_A_B_P|Col_A_B_P] by (apply Classical_Prop.classic).
+	{
+		(* case n_Col_A_B_P *)
+		pose proof (lemma_s_n_col_ncol _ _ _ n_Col_A_B_P) as nCol_A_B_P.
+
+		pose proof (proposition_15a _ _ _ _ _ BetS_A_B_C BetS_P_B_Q nCol_A_B_P) as CongA_ABP_QBC.
+
+		pose proof (lemma_equalanglesNC _ _ _ _ _ _ CongA_ABP_QBC) as nCol_Q_B_C.
+		pose proof (lemma_ABCequalsCBA _ _ _ nCol_Q_B_C) as CongA_QBC_CBQ.
+
+		pose proof (lemma_equalanglestransitive _ _ _ _ _ _ _ _ _ CongA_ABP_QBC CongA_QBC_CBQ) as CongA_ABP_CBQ.
+		pose proof (proposition_04 _ _ _ _ _ _ Cong_BA_BC Cong_BP_BQ CongA_ABP_CBQ) as (Cong_AP_CQ & _ & _).
+
+		exact Cong_AP_CQ.
+	}
+	(* case Col_A_B_P *)
+	apply Classical_Prop.NNPP in Col_A_B_P.
+
+	(* assert by cases *)
+	destruct Col_A_B_P as [eq_A_B | [eq_A_P | [eq_B_P | [BetS_B_A_P | [BetS_A_B_P | BetS_A_P_B]]]]].
+	{
+		(* case eq_A_B *)
+
+		contradict eq_A_B.
+		exact neq_A_B.
+	}
+	{
+		(* case eq_A_P *)
+		contradict eq_A_P.
+		exact neq_A_P.
+	}
+	{
+		(* case eq_B_P *)
+
+		contradict eq_B_P.
+		exact neq_B_P.
+	}
+	{
+		(* case BetS_B_A_P *)
+		pose proof (axiom_betweennesssymmetry _ _ _ BetS_B_A_P) as BetS_P_A_B.
+
+		pose proof (lemma_orderofpoints_ABC_BCD_ACD _ _ _ _ BetS_P_A_B BetS_A_B_C) as BetS_P_B_C.
+		pose proof (lemma_s_onray _ _ _ _ BetS_P_B_C BetS_P_B_Q) as OnRay_BC_Q.
+		pose proof (lemma_onray_ABC_ACB _ _ _ OnRay_BC_Q) as OnRay_BQ_C.
+
+		pose proof (lemma_s_lt _ _ _ _ _ BetS_B_A_P Cong_BA_CB) as Lt_CB_BP.
+		pose proof (lemma_lessthancongruence _ _ _ _ _ _ Lt_CB_BP Cong_BP_BQ) as Lt_CB_BQ.
+		pose proof (lemma_lessthancongruence_smaller _ _ _ _ _ _ Lt_CB_BQ Cong_CB_BC) as Lt_BC_BQ.
+
+		pose proof (proposition_03 _ _ _ _ _ _ Lt_BC_BQ Cong_BQ_BQ) as (H & BetS_B_H_Q & Cong_BH_BC).
+
+		pose proof (lemma_s_onray_assert_bets_AEB _ _ _ BetS_B_H_Q neq_B_Q) as OnRay_BQ_H.
+
+		pose proof (lemma_congruencesymmetric _ _ _ _ Cong_BH_BC) as Cong_BC_BH.
+
+		pose proof (lemma_layoffunique _ _ _ _ OnRay_BQ_C OnRay_BQ_H Cong_BC_BH) as eq_C_H.
+		assert (BetS B C Q) as BetS_B_C_Q by (rewrite eq_C_H; exact BetS_B_H_Q).
+
+		pose proof (lemma_differenceofparts _ _ _ _ _ _ Cong_BA_BC Cong_BP_BQ BetS_B_A_P BetS_B_C_Q) as Cong_AP_CQ.
+
+		exact Cong_AP_CQ.
+	}
+	{
+		(* case BetS_A_B_P *)
+		assert (BetS B P C \/ ~ BetS B P C) as [BetS_B_P_C|n_BetS_B_P_C] by (apply Classical_Prop.classic).
+		{
+			(* case BetS_B_P_C *)
+			pose proof (axiom_betweennesssymmetry _ _ _ BetS_B_P_C) as BetS_C_P_B.
+
+			pose proof (lemma_orderofpoints_ABC_BCD_ACD _ _ _ _ BetS_C_P_B BetS_P_B_Q) as BetS_C_B_Q.
+			pose proof (cn_sumofparts _ _ _ _ _ _ Cong_AB_CB Cong_BP_BQ BetS_A_B_P BetS_C_B_Q) as Cong_AP_CQ.
+
+			exact Cong_AP_CQ.
+		}
+		(* case n_BetS_B_P_C *)
+		assert (BetS B C P \/ ~ BetS B C P) as [BetS_B_C_P|n_BetS_B_C_P] by (apply Classical_Prop.classic).
+		{
+			(* case BetS_B_C_P *)
+			pose proof (axiom_orderofpoints_ABD_BCD_ABC _ _ _ _ BetS_Q_B_P BetS_B_C_P) as BetS_Q_B_C.
+
+			pose proof (axiom_betweennesssymmetry _ _ _ BetS_Q_B_C) as BetS_C_B_Q.
+			pose proof (cn_sumofparts _ _ _ _ _ _ Cong_AB_CB Cong_BP_BQ BetS_A_B_P BetS_C_B_Q) as Cong_AP_CQ.
+
+			exact Cong_AP_CQ.
+		}
+		(* case n_BetS_B_C_P *)
+
+		pose proof (lemma_outerconnectivity _ _ _ _ BetS_A_B_P BetS_A_B_C n_BetS_B_P_C n_BetS_B_C_P) as eq_P_C.
+		assert (Cong A B B P) as Cong_AB_BP by (rewrite eq_P_C; exact Cong_AB_BC).
+		assert (BetS P B A) as BetS_P_B_A by (rewrite eq_P_C; exact BetS_C_B_A).
+		assert (Cong A P C A) as Cong_AP_CA by (rewrite eq_P_C; exact Cong_AC_CA).
+
+		pose proof (lemma_congruencetransitive _ _ _ _ _ _ Cong_AB_BP Cong_BP_BQ) as Cong_AB_BQ.
+		pose proof (lemma_congruencesymmetric _ _ _ _ Cong_AB_BQ) as Cong_BQ_AB.
+		pose proof (lemma_congruenceflip _ _ _ _ Cong_BQ_AB) as (_ & _ & Cong_BQ_BA).
+
+		pose proof (lemma_extensionunique _ _ _ _ BetS_P_B_Q BetS_P_B_A Cong_BQ_BA) as eq_Q_A.
+		assert (Cong A P C Q) as Cong_AP_CQ by (rewrite eq_Q_A; exact Cong_AP_CA).
+
+		exact Cong_AP_CQ.
+	}
+	{
+		(* case BetS_A_P_B *)
+		pose proof (axiom_betweennesssymmetry _ _ _ BetS_A_P_B) as BetS_B_P_A.
+		pose proof (lemma_orderofpoints_ABC_ACD_BCD _ _ _ _ BetS_A_P_B BetS_A_B_C) as BetS_P_B_C.
+		pose proof (lemma_s_onray _ _ _ _ BetS_P_B_C BetS_P_B_Q) as OnRay_BC_Q.
+
+		pose proof (lemma_s_lt _ _ _ _ _ BetS_B_P_A Cong_BP_QB) as Lt_QB_BA.
+		pose proof (lemma_lessthancongruence _ _ _ _ _ _ Lt_QB_BA Cong_BA_BC) as Lt_QB_BC.
+		pose proof (lemma_lessthancongruence_smaller _ _ _ _ _ _ Lt_QB_BC Cong_QB_BQ) as Lt_BQ_BC.
+
+		pose proof (proposition_03 _ _ _ _ _ _ Lt_BQ_BC Cong_BC_BC) as (H & BetS_B_H_C & Cong_BH_BQ).
+
+		pose proof (lemma_s_onray_assert_bets_AEB _ _ _ BetS_B_H_C neq_B_C) as OnRay_BC_H.
+
+		pose proof (lemma_congruencesymmetric _ _ _ _ Cong_BH_BQ) as Cong_BQ_BH.
+
+		pose proof (lemma_layoffunique _ _ _ _ OnRay_BC_Q OnRay_BC_H Cong_BQ_BH) as eq_Q_H.
+		assert (BetS B Q C) as BetS_B_Q_C by (rewrite eq_Q_H; exact BetS_B_H_C).
+
+		pose proof (lemma_differenceofparts _ _ _ _ _ _ Cong_BP_BQ Cong_BA_BC BetS_B_P_A BetS_B_Q_C) as Cong_PA_QC.
+		pose proof (lemma_congruenceflip _ _ _ _ Cong_PA_QC) as (Cong_AP_CQ & _ & _).
+
+		exact Cong_AP_CQ.
+	}
+Qed.
+
+End Euclid.
---
+++ lemma_right_triangle_same_base_cong_side_cong_hypotenuse.v
@@ -0,0 +1,127 @@
+Require Import ProofCheckingEuclid.euclidean_axioms.
+Require Import ProofCheckingEuclid.euclidean_defs.
+Require Import ProofCheckingEuclid.lemma_congruenceflip.
+Require Import ProofCheckingEuclid.lemma_congruencesymmetric.
+Require Import ProofCheckingEuclid.lemma_congruencetransitive.
+Require Import ProofCheckingEuclid.lemma_doublereverse.
+Require Import ProofCheckingEuclid.lemma_extensionunique.
+Require Import ProofCheckingEuclid.lemma_inequalitysymmetric.
+Require Import ProofCheckingEuclid.lemma_interior5.
+Require Import ProofCheckingEuclid.lemma_linereflectionisometry.
+Require Import ProofCheckingEuclid.lemma_right_triangle_symmetric.
+Require Import ProofCheckingEuclid.lemma_rightreverse.
+Require Import ProofCheckingEuclid.lemma_s_right_triangle.
+Require Import ProofCheckingEuclid.proposition_10.
+
+Section Euclid.
+
+Context `{Ax:euclidean_neutral_ruler_compass}.
+
+(*
+	There are three notable scenarios here.
+	1. SS C H A B
+	2. OS C A B H
+	3. ~ SS C H A B /\ OS C A B H -> A B C H are not in the same plane.
+*)
+Lemma lemma_right_triangle_same_base_cong_side_cong_hypotenuse :
+	forall A B C H,
+	RightTriangle A B C ->
+	RightTriangle A B H ->
+	Cong B C B H ->
+	Cong A C A H.
+Proof.
+	intros A B C H.
+	intros RightTriangle_ABC.
+	intros RightTriangle_ABH.
+	intros Cong_BC_BH.
+
+	pose proof (cn_congruencereflexive A C) as Cong_AC_AC.
+
+	pose proof (lemma_right_triangle_symmetric _ _ _ RightTriangle_ABC) as RightTriangle_CBA.
+
+	assert (RightTriangle_ABC2 := RightTriangle_ABC).
+	destruct RightTriangle_ABC2 as (D & BetS_A_B_D & Cong_AB_DB & Cong_AC_DC & _).
+
+	pose proof (lemma_congruencesymmetric _ _ _ _ Cong_AB_DB) as Cong_DB_AB.
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_AC_DC) as (Cong_CA_CD & _ & _).
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_AB_DB) as (Cong_BA_BD & _ & _).
+
+	assert (RightTriangle_ABH2 := RightTriangle_ABH).
+	destruct RightTriangle_ABH2 as (F & BetS_A_B_F & Cong_AB_FB & Cong_AH_FH & _).
+
+	pose proof (lemma_congruencetransitive _ _ _ _ _ _ Cong_DB_AB Cong_AB_FB) as Cong_DB_FB.
+	pose proof (lemma_doublereverse _ _ _ _ Cong_DB_FB) as (Cong_BF_BD & _).
+
+	pose proof (lemma_doublereverse _ _ _ _ Cong_BC_BH) as (_ & Cong_CB_HB).
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_BC_BH) as (_ & Cong_CB_BH & _).
+
+	pose proof (lemma_extensionunique _ _ _ _ BetS_A_B_F BetS_A_B_D Cong_BF_BD) as eq_F_D.
+	assert (Cong A H D H) as Cong_AH_DH by (rewrite <- eq_F_D; exact Cong_AH_FH).
+
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_AH_DH) as (Cong_HA_HD & _ & _).
+	pose proof (lemma_congruencesymmetric _ _ _ _ Cong_AH_DH) as Cong_DH_AH.
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_DH_AH) as (Cong_HD_HA & _ & _).
+
+	(* assert by cases *)
+	assert (eq C H \/ neq C H) as [eq_C_H|neq_C_H] by (apply Classical_Prop.classic).
+	{
+		(* case eq_C_H *)
+		assert (Cong A C A H) as Cong_AC_AH by (rewrite <- eq_C_H; exact Cong_AC_AC).
+
+		exact Cong_AC_AH.
+	}
+	(* case neq_C_H *)
+	pose proof (proposition_10 _ _ neq_C_H) as (M & BetS_C_M_H & Cong_MC_MH).
+
+	pose proof (cn_congruencereflexive C M) as Cong_CM_CM.
+	pose proof (cn_congruencereflexive M H) as Cong_MH_MH.
+
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_MC_MH) as (Cong_CM_HM & _ & _).
+
+	(* assert by cases *)
+	assert (eq B M \/ neq B M) as [eq_B_M|neq_B_M] by (apply Classical_Prop.classic).
+	{
+		(* case eq_B_M *)
+		assert (BetS C B H) as BetS_C_B_H by (rewrite eq_B_M; exact BetS_C_M_H).
+		pose proof (lemma_rightreverse _ _ _ _ RightTriangle_CBA BetS_C_B_H Cong_CB_BH) as Cong_CA_HA.
+		pose proof (lemma_congruenceflip _ _ _ _ Cong_CA_HA) as (Cong_AC_AH & _ & _).
+
+		exact Cong_AC_AH.
+	}
+	(* case neq_B_M *)
+	pose proof (lemma_inequalitysymmetric _ _ neq_B_M) as neq_M_B.
+
+	pose proof (lemma_s_right_triangle _ _ _ _ BetS_C_M_H Cong_CM_HM Cong_CB_HB neq_M_B) as RightTriangle_CMB.
+	pose proof (lemma_right_triangle_symmetric _ _ _ RightTriangle_CMB) as RightTriangle_BMC.
+
+	(*
+		Exterior triangles are △ACH and △DCH
+		Shared interior sides are AM and DM.
+	*)
+	pose proof (
+		lemma_interior5
+		C M H A
+		C M H D
+
+		BetS_C_M_H
+		BetS_C_M_H
+		Cong_CM_CM
+		Cong_MH_MH
+		Cong_CA_CD
+		Cong_HA_HD
+	) as Cong_MA_MD.
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_MA_MD) as (Cong_AM_DM & _ & _).
+
+	pose proof (lemma_s_right_triangle _ _ _ _ BetS_A_B_D Cong_AB_DB Cong_AM_DM neq_B_M) as RightTriangle_ABM.
+	pose proof (lemma_right_triangle_symmetric _ _ _ RightTriangle_ABM) as RightTriangle_MBA.
+
+	(* Points C and A are reflected across line MB. *)
+	pose proof (lemma_linereflectionisometry _ _ _ _ _ _ RightTriangle_BMC RightTriangle_MBA BetS_C_M_H BetS_A_B_D Cong_MC_MH Cong_BA_BD) as Cong_CA_HD.
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_CA_HD) as (Cong_AC_DH & _ & _).
+	pose proof (lemma_congruencetransitive _ _ _ _ _ _ Cong_CA_HD Cong_HD_HA) as Cong_CA_HA.
+	pose proof (lemma_congruenceflip _ _ _ _ Cong_CA_HA) as (Cong_AC_AH & _ & _).
+
+	exact Cong_AC_AH.
+Qed.
+
+End Euclid.
