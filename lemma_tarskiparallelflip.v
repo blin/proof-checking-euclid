d9bcb52c3fec5cbe55c981cc1e30e6fc8876660e
Evgenii Petrov <blinunleius@gmail.com>
2023-07-06 15:15:35 +0000

    Prove proposition_30

---
+++ lemma_tarskiparallelflip.v
@@ -0,0 +1,118 @@
+Require Import ProofCheckingEuclid.euclidean_axioms.
+Require Import ProofCheckingEuclid.euclidean_defs.
+Require Import ProofCheckingEuclid.lemma_NCdistinct.
+Require Import ProofCheckingEuclid.lemma_NCorder.
+Require Import ProofCheckingEuclid.lemma_collinearorder.
+Require Import ProofCheckingEuclid.lemma_inequalitysymmetric.
+Require Import ProofCheckingEuclid.lemma_s_col_BetS_A_B_C.
+Require Import ProofCheckingEuclid.lemma_s_col_BetS_A_C_B.
+Require Import ProofCheckingEuclid.lemma_s_col_BetS_B_A_C.
+Require Import ProofCheckingEuclid.lemma_s_col_eq_A_B.
+Require Import ProofCheckingEuclid.lemma_s_col_eq_A_C.
+Require Import ProofCheckingEuclid.lemma_s_col_eq_B_C.
+Require Import ProofCheckingEuclid.lemma_s_isosceles.
+Require Import ProofCheckingEuclid.lemma_s_meet.
+Require Import ProofCheckingEuclid.lemma_s_n_col_ncol.
+Require Import ProofCheckingEuclid.lemma_s_ncol_n_col.
+Require Import ProofCheckingEuclid.lemma_s_ncol_triangle.
+Require Import ProofCheckingEuclid.lemma_s_oncirc_radius.
+Require Import ProofCheckingEuclid.lemma_s_onray_assert_ABB.
+Require Import ProofCheckingEuclid.lemma_s_onray_assert_bets_ABE.
+Require Import ProofCheckingEuclid.lemma_s_onray_assert_bets_AEB.
+Require Import ProofCheckingEuclid.lemma_s_ss.
+Require Import ProofCheckingEuclid.lemma_s_triangle.
+Require Import ProofCheckingEuclid.lemma_samesidesymmetric.
+
+Section Euclid.
+
+Context `{Ax:euclidean_neutral_ruler_compass}.
+
+Lemma lemma_tarskiparallelflip :
+	forall A B C D,
+	TarksiPar A B C D ->
+	TarksiPar B A C D /\ TarksiPar A B D C /\ TarksiPar B A D C.
+Proof.
+	intros A B C D.
+	intros TarksiPar_A_B_C_D.
+
+
+	destruct TarksiPar_A_B_C_D as (neq_A_B & neq_C_D & n_Meet_A_B_C_D & SS_C_D_A_B).
+	pose proof (lemma_samesidesymmetric _ _ _ _ SS_C_D_A_B) as (SS_D_C_A_B & _ & _).
+	pose proof (lemma_inequalitysymmetric _ _ neq_C_D) as neq_D_C.
+
+	assert (~ Meet A B D C) as n_Meet_A_B_D_C.
+	{
+		intro Meet_A_B_D_C.
+
+		destruct Meet_A_B_D_C as (T & _ & _ & Col_A_B_T & Col_D_C_T).
+		pose proof (lemma_collinearorder _ _ _ Col_D_C_T) as (Col_C_D_T & _ & _ & _ & _).
+		pose proof (lemma_s_meet _ _ _ _ _ neq_A_B neq_C_D Col_A_B_T Col_C_D_T) as Meet_A_B_C_D.
+		contradict Meet_A_B_C_D.
+		exact n_Meet_A_B_C_D.
+	}
+
+	assert (TarksiPar A B D C) as TarksiPar_A_B_D_C.
+	unfold TarksiPar.
+	split.
+	exact neq_A_B.
+	split.
+	exact neq_D_C.
+	split.
+	exact n_Meet_A_B_D_C.
+	exact SS_D_C_A_B.
+
+	assert (~ Meet B A C D) as n_Meet_B_A_C_D.
+	{
+		intro Meet_B_A_C_D.
+
+		destruct Meet_B_A_C_D as (T & neq_B_A & _ & Col_B_A_T & Col_C_D_T).
+		pose proof (lemma_collinearorder _ _ _ Col_B_A_T) as (Col_A_B_T & _ & _ & _ & _).
+		pose proof (lemma_s_meet _ _ _ _ _ neq_A_B neq_C_D Col_A_B_T Col_C_D_T) as Meet_A_B_C_D.
+		contradict Meet_A_B_C_D.
+		exact n_Meet_A_B_C_D.
+	}
+
+	pose proof (lemma_inequalitysymmetric _ _ neq_A_B) as neq_B_A.
+	pose proof (lemma_samesidesymmetric _ _ _ _ SS_C_D_A_B) as (_ & SS_C_D_B_A & _).
+	pose proof (lemma_samesidesymmetric _ _ _ _ SS_C_D_B_A) as (SS_D_C_B_A & _ & _).
+
+	assert (TarksiPar B A C D) as TarksiPar_B_A_C_D.
+	unfold TarksiPar.
+	split.
+	exact neq_B_A.
+	split.
+	exact neq_C_D.
+	split.
+	exact n_Meet_B_A_C_D.
+	exact SS_C_D_B_A.
+
+	assert (~ Meet B A D C) as n_Meet_B_A_D_C.
+	{
+		intro Meet_B_A_D_C.
+
+		destruct Meet_B_A_D_C as (T & _ & _ & Col_B_A_T & Col_D_C_T).
+		pose proof (lemma_collinearorder _ _ _ Col_B_A_T) as (Col_A_B_T & _ & _ & _ & _).
+		pose proof (lemma_collinearorder _ _ _ Col_D_C_T) as (Col_C_D_T & _ & _ & _ & _).
+		pose proof (lemma_s_meet _ _ _ _ _ neq_A_B neq_C_D Col_A_B_T Col_C_D_T) as Meet_A_B_C_D.
+		contradict Meet_A_B_C_D.
+		exact n_Meet_A_B_C_D.
+	}
+
+	assert (TarksiPar B A D C) as TarksiPar_B_A_D_C.
+	unfold TarksiPar.
+	split.
+	exact neq_B_A.
+	split.
+	exact neq_D_C.
+	split.
+	exact n_Meet_B_A_D_C.
+	exact SS_D_C_B_A.
+
+	split.
+	exact TarksiPar_B_A_C_D.
+	split.
+	exact TarksiPar_A_B_D_C.
+	exact TarksiPar_B_A_D_C.
+Qed.
+
+End Euclid.
