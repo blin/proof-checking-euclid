71540bb1a871b0b44be5d76cb3bd6c43198046bd
Evgenii Petrov <blinunleius@gmail.com>
2023-08-11 20:33:23 +0100

    Add by_prop_CongA_flip_(left|right)

--- by_prop_CongA_flip.v
+++ by_prop_CongA_flip.v
@@ -1,8 +1,5 @@
-Require Import ProofCheckingEuclid.by_prop_CongA_ABCequalsCBA.
-Require Import ProofCheckingEuclid.by_prop_CongA_NC.
-Require Import ProofCheckingEuclid.by_prop_CongA_symmetric.
-Require Import ProofCheckingEuclid.by_prop_CongA_transitive.
-Require Import ProofCheckingEuclid.by_prop_nCol_order.
+Require Import ProofCheckingEuclid.by_prop_CongA_flip_left.
+Require Import ProofCheckingEuclid.by_prop_CongA_flip_right.
 Require Import ProofCheckingEuclid.euclidean_axioms.
 Require Import ProofCheckingEuclid.euclidean_defs.
 
@@ -18,16 +15,8 @@ Proof.
 	intros A B C D E F.
 	intros CongA_ABC_DEF.
 
-	pose proof (by_prop_CongA_NC _ _ _ _ _ _ CongA_ABC_DEF) as nCol_D_E_F.
-	pose proof (by_prop_CongA_symmetric _ _ _ _ _ _ CongA_ABC_DEF) as CongA_DEF_ABC.
-	pose proof (by_prop_CongA_NC _ _ _ _ _ _ CongA_DEF_ABC) as nCol_A_B_C.
-
-	pose proof (by_prop_nCol_order _ _ _ nCol_A_B_C) as (_ & _ & _ & _ & nCol_C_B_A).
-
-	pose proof (by_prop_CongA_ABCequalsCBA _ _ _ nCol_C_B_A) as CongA_CBA_ABC.
-	pose proof (by_prop_CongA_transitive _ _ _ _ _ _ _ _ _ CongA_CBA_ABC CongA_ABC_DEF) as CongA_CBA_DEF.
-	pose proof (by_prop_CongA_ABCequalsCBA _ _ _ nCol_D_E_F) as CongA_DEF_FED.
-	pose proof (by_prop_CongA_transitive _ _ _ _ _ _ _ _ _ CongA_CBA_DEF CongA_DEF_FED) as CongA_CBA_FED.
+	pose proof (by_prop_CongA_flip_left _ _ _ _ _ _ CongA_ABC_DEF) as CongA_CBA_DEF.
+	pose proof (by_prop_CongA_flip_right _ _ _ _ _ _ CongA_CBA_DEF) as CongA_CBA_FED.
 
 	exact CongA_CBA_FED.
 Qed.
